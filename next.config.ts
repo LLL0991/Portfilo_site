1a86072b11358afcaa3b0ab5f76b4d8c82c55b20
LLL <darays0903@gmail.com>
2026-06-25 01:51:58 +0800

    Reduce stylize function trace size

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
+  outputFileTracingExcludes: {
+    "/api/stylize": ["./public/**/*"],
+    "/api/stylize/download": ["./public/**/*"],
+  },
+  outputFileTracingIncludes: {
+    "/api/stylize": ["./public/images/home/stylize/styles/**/*"],
+  },
 };
 
 export default withNextIntl(nextConfig);
